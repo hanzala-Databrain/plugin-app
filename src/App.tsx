40518fa339074c9e1d477664d97533553913fc9e
asimaries <logxtech59@gmail.com>
2025-01-01 20:43:20 +0530

    fix: adding embeding

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-full">
-      <dbn-dashboard
-        token={"feee48ee-7d16-4bf6-896d-deadbfb99f3f"}
-        theme={JSON.stringify({
-          colors: {
-            primary: "#fff",
-          },
-        })}
-        options={JSON.stringify({
-          hideDashboardName: false,
-          disableMetricCreation: false,
-          disableScheduleEmailReports: false,
-          disableLayoutCustomization: false,
-          disableManageMetrics: false,
-        })}
-        dashboard-id={"plugin-test"}
-        handle-server-event="databrainServerEvent"
-      />
+    <div>
+      <iframe
+        src="https://stackblitz.com/edit/databrain-demo?embed=1&file=src%2FApp.tsx&hideExplorer=1&view=both&terminalHeight=1&hidedevtools=0"
+        style={{
+          width: "90vw",
+          height: "90vh",
+        }}
+      ></iframe>
     </div>
   );
 }
