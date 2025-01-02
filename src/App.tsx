de6eadf7f5e2f18033ba3f2b1b8b1c83d2d210ed
hanzala-Databrain <hanzala@usedatabrain.com>
2025-01-02 20:58:34 +0530

    fix: added dashboard and metric

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <iframe
-        src="https://stackblitz.com/edit/databrain-demo?embed=1&file=src%2FApp.tsx&hideExplorer=1&view=both&terminalHeight=1&hidedevtools=0"
+        src="https://stackblitz.com/edit/databrain-demo?embed=1&file=src%2Fcomponents%2FDashboard.tsx,src%2Fcomponents%2FMetric.tsx&hideExplorer=1&view=both&terminalHeight=1&hidedevtools=0"
         style={{
           width: "90vw",
           height: "90vh",
